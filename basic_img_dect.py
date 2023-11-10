ff761a6e34852879c443a971f52b90bee0c0ebad
Jonathan Müller <jona98welkers@web.de>
2023-11-11 00:18:31 +0100

    create basic heatmap

--- basic_img_dect.py
+++ basic_img_dect.py
@@ -1,9 +1,11 @@
 import cv2 as cv
 import numpy as np 
 from ultralytics import YOLO
+from create_heatmap import crt_htmp
 
 model = YOLO("yolov8s-seg.pt")
 vid = cv.VideoCapture(0)
+allBB = []
 
 while(True):
     
@@ -11,23 +13,16 @@ while(True):
     frame  = cv.resize(frame, (640,480))
     
     results = model(frame, conf=0.1, classes=[0], retina_masks=True)
-    try:
-        masks = results[0].masks.data
-        blurFrame = cv.GaussianBlur(frame,[101,101],0)
-        
-        for mask in masks:
-            bitMask = mask.numpy()
-            bitMask = np.around(bitMask)
-            
-            
-            frame[bitMask == 1] = blurFrame[bitMask == 1]
-      
-    except Exception as e:
-          print(e)
+    boxes = results[0].boxes.xyxy
+    
+    for box in boxes:
+        allBB.append(box.numpy())
         
     # Display the resulting frame 
-    cv.imshow('frame', frame) 
-      
+    #cv.imshow('frame', frame) 
+    
+    crt_htmp(frame, allBB)
+    
     # the 'q' button is set as the 
     # quitting button you may use any 
     # desired button of your choice 
