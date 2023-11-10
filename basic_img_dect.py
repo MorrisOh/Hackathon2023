593c3e8f03dc54dacc4572af8aba9c97e7212e6c
Jonathan Müller <jona98welkers@web.de>
2023-11-10 20:42:06 +0100

    Bluring shit

--- basic_img_dect.py
+++ basic_img_dect.py
@@ -3,21 +3,30 @@ import numpy as np
 from ultralytics import YOLO
 
 model = YOLO("yolov8s-seg.pt")
-vid = cv.VideoCapture(1)
+vid = cv.VideoCapture(0)
 
 while(True):
     
     ret, frame = vid.read() 
-    frame  = cv.resize(frame, [640,480])
+    frame  = cv.resize(frame, (640,480))
     
     results = model(frame, conf=0.1, classes=[0], retina_masks=True)
-    masks = results[0].masks
-    
-    #for mask in masks:
+    try:
+        masks = results[0].masks.data
+        blurFrame = cv.GaussianBlur(frame,[101,101],0)
         
-    
+        for mask in masks:
+            bitMask = mask.numpy()
+            bitMask = np.around(bitMask)
+            
+            
+            frame[bitMask == 1] = blurFrame[bitMask == 1]
+      
+    except Exception as e:
+          print(e)
+        
     # Display the resulting frame 
-    cv.imshow('frame', results[0].plot()) 
+    cv.imshow('frame', frame) 
       
     # the 'q' button is set as the 
     # quitting button you may use any 
