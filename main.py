2052f49bd20d8ec6acc2b77c8f325f3475dfbc22
negralessio <Alessio.Negrini@live.de>
2023-11-11 19:58:36 +0100

    Added argparser for CLI arguments support

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from src.vision import Vision
 
 
 def main():
+    # Parse args
+    args = utils.parse_args()
+
+    # Get logger
     logger = utils.get_logger()
 
     # Create Vision object with YOLO model located in root
     vision = Vision(model=utils.get_model(),
                     path_to_video="data/raw/test_video.mov",
-                    confidence_threshold=0.3,
+                    confidence_threshold=args["conf"],
                     logger=logger)
 
     # Get frames to process and create heatmap
-    centers, image, frame_count = vision.get_frames(take_every_k_frame=24)
+    centers, image, frame_count = vision.get_frames(take_every_k_frame=args["k"])
     heatmap_overlayed, heatmap = vision.get_heatmap()
+
     # Plot and/or save results
     vision.plot_densities(savefig=True, showfig=False)
 
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch.backends.mps
 from ultralytics import YOLO
 
@@ -17,6 +19,33 @@ def get_model():
 
 
 def get_logger(log_level=logging.INFO):
+    """ Configures the logger and returns a logger object
+
+    :param log_level: constant -- Level of Log
+    :return: logger object
+    """
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=log_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     return logger
+
+
+def parse_args() -> dict:
+    """ Sets the CLI arguments and parses it
+
+    :return: vars(args): dict -- Parsed args as dict
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    # Argument 1: Every k frame to consider
+    parser.add_argument("--k",
+                        default=20,
+                        type=int,
+                        help="int -- Every k-th you want to consider during inference")
+    # Argument 2: Confidence Threshold
+    parser.add_argument("--conf",
+                        default=0.3,
+                        type=float,
+                        help="float -- Confidence threshold to consider for classification")
+    # places the extracted data in an argparse.Namespace object:
+    args = parser.parse_args()
+    return vars(args)
