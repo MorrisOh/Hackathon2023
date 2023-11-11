ea527afbc3a133fdbff447548b4304b5d842a755
negralessio <Alessio.Negrini@live.de>
2023-11-11 16:52:28 +0100

    Improved docs

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import src.utils as utils
 
 from src.vision import Vision
@@ -8,13 +6,16 @@ from src.vision import Vision
 def main():
     logger = utils.get_logger()
 
+    # Create Vision object with YOLO model located in root
     vision = Vision(model=utils.get_model(),
                     path_to_video="data/raw/test_video.mov",
                     confidence_threshold=0.3,
                     logger=logger)
 
+    # Get frames to process and create heatmap
     centers, image, frame_count = vision.get_frames(take_every_k_frame=24)
-    heatmap: np.array = vision.get_heatmap()
+    heatmap_overlayed, heatmap = vision.get_heatmap()
+    # Plot and/or save results
     vision.plot_densities(savefig=True, showfig=False)
 
 
