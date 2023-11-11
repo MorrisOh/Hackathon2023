1f51b2c5f38ef5c1a177f42c2d5e5220fcbef8c6
negralessio <Alessio.Negrini@live.de>
2023-11-11 16:32:40 +0100

    Minor changes

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from src.vision import Vision
 def main():
     logger = utils.get_logger()
 
-    vision = Vision(model=utils.get_model('yolov8s.pt'),
+    vision = Vision(model=utils.get_model(),
                     path_to_video="data/raw/test_video.mov",
                     confidence_threshold=0.3,
                     logger=logger)
 
-    centers, image, frame_count = vision.get_frames(take_every_k_frame=100)
+    centers, image, frame_count = vision.get_frames(take_every_k_frame=24)
     heatmap: np.array = vision.get_heatmap()
-    vision.plot_densities()
+    vision.plot_densities(savefig=True, showfig=False)
 
 
 if __name__ == "__main__":
